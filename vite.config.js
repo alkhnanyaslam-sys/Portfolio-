c6e722f914c67641e2be135018911e952fce7cb3
alkhnanyaslam-sys <alkhnanyaslam@gmail.com>
2026-08-15 05:23:56 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// ⚠️ IMPORTANT: change "REPO_NAME" to your exact GitHub repository name.
-// If your repo is https://github.com/USERNAME/islam-portfolio
-// then base should be "/islam-portfolio/"
 export default defineConfig({
-  base: "/REPO_NAME/",
+  base: "/Portfolio-/",
   plugins: [react()],
 });
